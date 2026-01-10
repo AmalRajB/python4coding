5fda966957d52a8ee29835fe7108213fdb9d2432
AmalRajB <ramal37204@gmail.com>
2026-01-10 14:05:43 +0530

    Implement function to convert lists to dictionary and display as tuples

---
+++ converting2list_into_dist.py
@@ -0,0 +1,15 @@
+def list_to_dist():
+    list1  = [1,2,3]
+    list2 = ['one','two','three']
+
+    result = dict(zip(list1,list2))
+    # displaying in the form of tuple 
+    for i in result.items():
+        print(i)
+    
+list_to_dist()    
+
+
+   
+
+
