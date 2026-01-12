b6ad8b3a706349daf0305e42cc6d138a4e3b5d36
AmalRajB <ramal37204@gmail.com>
2026-01-12 14:14:55 +0530

    Add function to find leap years within a specified range

--- leepyear_or_not.py
+++ leepyear_or_not.py
@@ -16,3 +16,29 @@ chek_leep_year(year)
 # out
 
 # 2024 is a leep year
+
+
+# leep years in an interver
+
+start = 2000
+end = 2020
+
+def checkleep(start,end):
+    for yer in range(start,end+1):
+        if (yer%400 ==0) and (yer%100 ==0):
+            print(yer)
+        elif (yer%4 == 0) and (yer%100 !=0):
+            print(yer)
+        else:
+            pass
+checkleep(start,end)               
+
+# out
+
+# 2000
+# 2004
+# 2008
+# 2012
+# 2016
+# 2020
+
