bbd292c9c4e3b4c5c0fc810637d17216632009c2
AmalRajB <ramal37204@gmail.com>
2026-01-13 19:26:06 +0530

    Add multiplication table generator function

---
+++ multiplication_table.py
@@ -0,0 +1,23 @@
+number = 3
+
+def multiplication(number):
+    for i in range(1,13):
+        result = i*number
+        print (f'{i} * {number} = {result}')   
+
+multiplication(number)         
+
+# out:
+
+# 1 * 3 = 3
+# 2 * 3 = 6
+# 3 * 3 = 9
+# 4 * 3 = 12
+# 5 * 3 = 15
+# 6 * 3 = 18
+# 7 * 3 = 21
+# 8 * 3 = 24
+# 9 * 3 = 27
+# 10 * 3 = 30
+# 11 * 3 = 33
+# 12 * 3 = 36
