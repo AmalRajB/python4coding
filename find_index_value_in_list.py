8022ae39373c36afd9b56d9206f63bdca9ffdcf0
AmalRajB <ramal37204@gmail.com>
2026-01-18 13:01:59 +0530

    Add example to find index and value in a list using enumerate

---
+++ find_index_value_in_list.py
@@ -0,0 +1,14 @@
+mylist = [2,33,5,66,22,11]
+
+for index , value in enumerate(mylist):
+    print(index , value)
+
+
+    # out:
+
+    # 0 2
+    # 1 33
+    # 2 5
+    # 3 66
+    # 4 22
+    # 5 11
