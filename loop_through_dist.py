ce9284bec30599035e5e79ea186e58146d4391a9
AmalRajB <ramal37204@gmail.com>
2026-01-19 13:20:27 +0530

    Add example to demonstrate looping through a dictionary using items() and keys()

---
+++ loop_through_dist.py
@@ -0,0 +1,17 @@
+mydict  = {'name':'amal',
+           'age':20,
+           'course':'BCA'}
+
+for key , value in mydict.items():
+    print(key, ":",value)
+
+
+# without the items() method
+for key in mydict:
+    print(key,":",mydict[key])
+    
+
+    # out:
+    # name : amal
+    # age : 20
+    # course : BCA
