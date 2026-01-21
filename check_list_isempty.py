b39b8d9e9955280822cb585e779409a4fe2ef46c
AmalRajB <ramal37204@gmail.com>
2026-01-21 14:39:00 +0530

    Add script to check if a list is empty using two methods

---
+++ check_list_isempty.py
@@ -0,0 +1,15 @@
+
+# method 1
+my_list = [1]
+
+if not my_list:
+    print('list is emplty')
+else:
+    print('list is not empty') 
+
+# method 2
+length = len(my_list)
+if length == 0:
+    print('empty')
+else:
+    print('not empty') 
