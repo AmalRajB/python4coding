097bae516647aa852dfc8e5f7f1a260a18f4cbed
AmalRajB <ramal37204@gmail.com>
2026-02-16 22:40:20 +0530

    Add initial implementation of vowel counting, word frequency, palindrome check, Armstrong number check, and range-based Armstrong number identification

---
+++ practice.py
@@ -0,0 +1,106 @@
+# count vovals
+
+# voval = ['a','e','i','o','u']
+
+# mystring = 'amal is a software developer'
+# mystring.casefold()
+# mystring.split()
+# mydist = {}
+# for i in mystring:
+#     if i in voval:
+#         if i not in mydist.keys():
+#             mydist[i] = 0
+#         mydist[i] +=1
+# print(mydist)            
+
+# word frequency
+
+
+# my_word = 'amalrajb'
+
+# my_dist = {}
+
+# my_word.split()
+
+# for i in my_word:
+#     if i not in my_dist.keys():
+#         my_dist[i] = 0
+#     my_dist[i]+=1
+
+# print(my_dist)        
+
+# palindrome
+
+
+
+# my_value = 121
+
+# check = str(my_value)
+
+# res = check[::-1]
+
+# if(res == check):
+#     print('palindrome')
+# else:
+#     print('not palindrome')    
+
+# armstrrong or not
+
+# number = 153
+
+# length = len(str(number))
+
+# total = 0
+
+# temp = number
+
+# while temp>0:
+#     digit = temp%10
+#     total += digit ** length
+#     temp = temp//10
+
+# if(total == number):
+#     print('number is armstrong')
+# else:
+#     print('number not a armstrong')       
+
+
+# in specified range
+
+
+# start = 100
+# end = 1000
+
+# for number in range(start,end+1):
+
+#     length = len(str(number))
+
+#     total = 0
+
+#     temp = number
+
+#     while temp>0:
+#         digit = temp%10
+#         total += digit ** length
+#         temp = temp//10
+#     if total == number:
+#         print(number)
+#     else:
+#         pass        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
