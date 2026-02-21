b83aefbb038c32d1b30c1624537028afb462605c
AmalRajB <ramal37204@gmail.com>
2026-02-21 20:47:11 +0530

    Add implementation of string reversal for both individual strings and list items

---
+++ string_reverse.py
@@ -0,0 +1,23 @@
+string = "amal"
+
+x = string[::-1]
+print(x)
+
+# reverse a  list item
+
+
+mylist = ['amal','athul',2]
+
+# mylist.reverse()
+# print(mylist)
+
+res = []
+for item in mylist:
+    res.insert(0,str(item)[::-1])
+print(res)
+
+# reversing both list and each items
+
+
+
+
